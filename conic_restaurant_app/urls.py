601d7c261b0a0a54be2913d74f7a88e8666a6dbc
hexaalif <shadullahsad9@gmail.com>
2023-12-20 19:38:03 +0600

    full production ready or assignment complete

--- conic_restaurant_app/urls.py
+++ conic_restaurant_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name = 'home'),
+    path('about/', views.about, name='about'),
+    path('items/', views.items, name='items')
 ]
